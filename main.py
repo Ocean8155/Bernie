ab8bd3ae4fc3afbd7993f1082f9381f96f079a2f
Ocean Lascano <>
2022-08-20 13:28:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# youtuber = "Marcos Lascano"
+# print("Subscribe to " +youtuber)
+# print("Subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb 1: ")
+verb2 = input("Verb 2: ")
+famous_person = input("Famous person: ")
+madlib = f"Computer Science is so {adj}! It makes me excited all the time \ because I love to {verb1}.Stay hydrated and {verb2} like you are {famous_person}!"
+print(madlib)
